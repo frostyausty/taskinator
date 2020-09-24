0841564c8b3ddb77b6cb44c128455cc0fe83fdee
frostyausty <priceaustin62@gmail.com>
2020-09-23 23:44:17 -0500

    added alert so form can't submit blank, form clears after task is added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,19 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    //clears out form after a task is added
+    formEl.reset();
 
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
-
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 };
@@ -19,7 +25,6 @@ var createTaskEl = function(taskDataObj) {
     //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
     //create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task=info";
