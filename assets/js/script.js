a28f9fb605950abfae30c3937a0dec17fcbd1c47
frostyausty <priceaustin62@gmail.com>
2020-09-29 16:49:34 -0500

    added delete functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEL = document.querySelector("#page-content");
 var taskIdCounter=0;
 
 var taskFormHandler = function(event) {
@@ -89,5 +90,21 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        //get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEL.addEventListener("click", taskButtonHandler);
 
